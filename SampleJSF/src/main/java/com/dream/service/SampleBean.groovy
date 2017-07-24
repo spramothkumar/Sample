8bfcd7defca80ce9d63cd1f5e1215eaa1241ec99
Pramothkumar S <pramoth.kumar@yantraservices.com>
2017-07-24 19:24:57 +0530

    Initial Commite

---
+++ SampleJSF/src/main/java/com/dream/service/SampleBean.groovy
@@ -0,0 +1,24 @@
+package com.dream.service;
+
+import javax.annotation.PostConstruct
+
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+
+import groovy.util.logging.Slf4j
+
+@Service
+@Scope("session")
+public class SampleBean {
+
+	String name;
+
+	Long age;
+
+	public String submitForm(){
+		println ("Submit form Called with data Name : "+name+" and Age "+age)
+		return "/data.xhtml"
+	}
+}
